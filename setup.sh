87ea394e494ad633678e233744d6d339d977917f
Akhila Gouda <84775199+goudaakhila@users.noreply.github.com>
2021-06-23 00:31:08 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
 mkdir -p ~/.streamlit/
 echo "\
-[general]\n\
-email = \"akhila9040@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-ehco "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
-port=$PORT\n\
-" >~/.streamlit/config.toml
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
